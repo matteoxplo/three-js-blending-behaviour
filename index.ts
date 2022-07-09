a1c936ddc1a347b8ab9d3ecebb087328b8feb962
matteoxplo <m.vavassori@xplo.com>
2022-07-09 11:18:43 +0200

    1 file modified, 4 added, 4 deleted

--- index.ts
+++ index.ts
@@ -1,13 +1,22 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { RoomEnvironment } from 'three/examples/jsm/environments/RoomEnvironment';
-
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+import { createMultiMaterialObject } from 'three/examples/jsm/utils/SceneUtils';
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+renderer.outputEncoding = THREE.sRGBEncoding;
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 
 const pmremGenerator = new THREE.PMREMGenerator(renderer);
 // const environment = new EnvironmentSceneAlt();
@@ -20,15 +29,21 @@ const env = pmremGenerator.fromScene(environment, 0.04).texture;
 scene.environment = env;
 scene.background = env;
 
+const texLoader = new THREE.TextureLoader();
+
+const supportTexture = texLoader.load(' textures/support.png ');
+
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
+const material = new THREE.MeshStandardMaterial({
+  color: 0xffffff,
+  map: supportTexture
+});
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 camera.position.z = 5;
 
 const controls = new OrbitControls(camera, renderer.domElement);
-
 
 const animate = function () {
   requestAnimationFrame(animate);
